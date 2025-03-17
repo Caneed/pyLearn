c1d73207b5b34e3644c5c55b792b0177afc10bc5
dedsec <1115659949@qq.com>
2025-03-17 16:04:01 +0800

    python learn

--- code/基础语法.py
+++ code/基础语法.py
@@ -1,2 +1,70 @@
-print(ord("A"))
-print(chr(25991))
+# print(ord("A"))
+# print(chr(25991))
+# animals =['cat','dog','fish']
+# print(animals)
+# animals.append('elephant')
+# print(animals)
+
+# animals=('cat','dog','fish')
+
+# pul=(1,)
+
+# print(pul)
+# print(type(pul))
+
+# t=(1,2,['A','B'])
+# print(t)
+# t[2][0]='X'
+# t[2][1]='Y'
+# print(t)
+
+# # (1, 2, ['A', 'B'])
+# # (1, 2, ['X', 'Y'])
+
+# age=20
+
+# if age>19:
+#     print('adult')
+# else:
+#     print('teenager')
+
+# # adult
+
+# score = "A"
+
+# match score:
+#     case "A":
+#         print("85-100")
+#     case "B":
+#         print("70-84")
+#     case "C":
+#         print("60-69")
+#     case "D":
+#         print("0-59")
+
+
+# age = 20
+
+# match age:
+#   case age if age > 19:
+#       print('adult')
+#   case 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18:
+#       print('teenager')
+#   case _:
+#       print('kid')
+
+# name=['mike','bob','tracy']
+
+# for i in name:
+#     print(i)
+# #     mike
+# # bob
+# # tracy
+
+sum=0
+
+for i in range(100):
+    sum+=i
+print(sum)
+
+# 4950
